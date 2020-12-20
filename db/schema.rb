031fc0eb52d330561b7c5fddbd5d5c3d493f7925
Josh Seckman <jseckman90@gmail.com>
2020-12-19 20:37:07 -0500

    created orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_225250) do
+ActiveRecord::Schema.define(version: 2020_12_20_005154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -32,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_12_19_225250) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "users"
 end
