3588d773bb7c5884e89a78ac12d7e9f3c7c4aa85
Josh Seckman <jseckman90@gmail.com>
2020-12-20 12:34:22 -0500

    seeded more products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+##################################
+############# USERS ##############
+##################################
 # user = User.create(username: "test", password: "test")
 
-Product.create(name:"tumbler", description:"stainless steel tumbler with dot art", img:"https://mobileimages.lowes.com/product/converted/888830/888830021811.jpg", qty:24, price: 64.99)
+
+##################################
+########### PRODUCTS #############
+##################################
+# Product.create(name:"tumbler navy", description:"stainless steel tumbler - navy", img:"https://mobileimages.lowes.com/product/converted/888830/888830040713.jpg?size=pdhi", qty:24, price: 64.99)
+# Product.create(name:"tumbler black", description:"stainless steel tumbler - black", img:"https://mobileimages.lowes.com/product/converted/888830/888830021835.jpg?size=pdhi", qty:8, price: 54.99)
+# Product.create(name:"tumbler seafoam", description:"stainless steel tumbler - seafoam", img:"https://mobileimages.lowes.com/product/converted/888830/888830021811.jpg?size=pdhi", qty:12, price: 39.99)
+# Product.create(name:"tumbler coral", description:"stainless steel tumbler - coral", img:"http://mobileimages.lowes.com/product/converted/888830/888830040683_14617813.jpg?size=pdhi", qty:6, price: 59.99)
+# Product.create(name:"tumbler green", description:"stainless steel tumbler - green", img:"http://mobileimages.lowes.com/product/converted/888830/888830080085_15833913.jpg?size=pdhi", qty:12, price: 49.99)
+# Product.create(name:"tumbler graphite", description:"stainless steel tumbler with - graphite", img:"http://mobileimages.lowes.com/product/converted/888830/888830076224_16243709.jpg?size=pdhi", qty:18, price: 39.99)
+# Product.create(name:"tumbler copper", description:"stainless steel tumbler - copper", img:"http://mobileimages.lowes.com/product/converted/888830/888830075838_16243699.jpg?size=pdhi", qty:20, price: 29.99)
+
